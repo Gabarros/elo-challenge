b6820f7267d3cb75c6aee657ac520d1df1fc53f8
Gabarros <gabarros17@gmail.com>
2020-03-09 20:45:14 -0300

    Finished tasks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-document.querySelector('#submit').addEventListener('click',
+const submitButton = document.querySelector('#submit');
+
+submitButton.addEventListener('click',
   function (event) {
     event.preventDefault();
     handleSubmit();
@@ -20,7 +22,13 @@ document.querySelector('#social-no').addEventListener('click', () => {
 
 function handleSubmit() {
   const name = document.querySelector('#name').value;
+
+  validateName(name);
+
   const telephone = document.querySelector('#telephone').value;
+  
+  validatePhone(telephone);
+  
   const meetDescription = document.querySelector('#meet-description').value;
   let socialNetworks = [];
   let formData = {};
@@ -50,6 +58,27 @@ function handleSubmit() {
 
 };
 
+function validateName(name){
+  var regName = /[a-zA-Z]+ [a-zA-Z]*/;
+
+  if(!regName.test(name)){
+      alert('Insira seu nome completo (Nome e Sobrenome)!');
+      document.getElementById('name').focus();
+      return false;
+  }
+};
+
+function validatePhone(phone){
+  var regPhone = /[0-9]{2}-[0-9]{9}/;
+
+  if(!regPhone.test(phone)){
+      alert('O telefone deve seguir o formato: XX-XXXXXXXX!');
+      document.getElementById('telephone').focus();
+      return false;
+  }
+
+}
+
 async function handlePost(formData){
 
   const Http = new XMLHttpRequest();
@@ -60,7 +89,8 @@ async function handlePost(formData){
 
   Http.onreadystatechange = function() { 
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        console.log("Sucess");
+        console.log("Success");
+        submitButton.disabled = true;
     }
 }
 }
